0f06df30164c19c8dda8e53587043789fb68923d
Manik1002 <manikmukhija@capxindia.in>
2024-10-19 13:30:22 +0530

    11h commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,21 @@ const Footer = () => {
                         <a href="#contact">Contact</a>
                     </li>
                 </ul>
+                
+                <div className="contact-info">
+                    <p>Phone: <a href="tel:+91 705-722-2020">+91 705-722-2020</a></p>
+                    <p>Email: <a href="mailto:contact@capxindia.in">contact@capxindia.in</a></p>
+                </div>
+                
+                <div className="social-media">
+                    <a href="https://www.facebook.com/" className="social-icon"><i className="fab fa-facebook-f"></i></a>
+                    <a href="https://x.com/i/flow/login" className="social-icon"><i className="fab fa-twitter"></i></a>
+                    <a href="https://www.instagram.com/" className="social-icon"><i className="fab fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/company/capx-india/" className="social-icon"><i className="fab fa-linkedin-in"></i></a>
+                </div>
+
                 <hr />
+                
                 <div className="bottom">
                     <p>&copy; 2024 Execute, Inc. All rights reserved.</p>
                 </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,8 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className='content'>
-                <p>Call us</p>
-                <p>1-800-123-4567</p>
+                <p>Welcome to</p>
+                <p>CAPXINDIA</p>
                 <p>We Believe</p>
                 <p>In Analysis and not forecasting</p>
                 <button href='/' className='button'>Login/SignUp</button>
